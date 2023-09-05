e43897e53ad4d000b0309f94cac6e822fbbf7886
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-05 10:35:52 -0500

    created client.js, data.js, index.js, and jquery vendor. linked all files

--- client.js
+++ client.js
@@ -1,1 +1,12 @@
-console.log('Here are all the available people:', people);
+$(onReady);
+
+console.log("Here are all the available people:", people);
+
+function onReady() {
+  console.log("hello world");
+
+  for (let i = 0; i < people.length; i++) {
+    let person = people[i];
+    console.log(person[i]);
+  }
+}
--- data.js
+++ data.js
@@ -1,10 +1,12 @@
 let people = [
   {
-    name: 'Blaine',
-    githubUsername: 'booherbg'
+    name: "Kenny",
+    githubUsername: "Kennyfergy",
+    image: "https://github.com/kennyfergy.png?size=250",
   },
   {
-    name: 'Mason',
-    githubUsername: 'masonleonhart'
-  }
+    name: "Joe",
+    githubUsername: "jcolago",
+    image: "https://github.com/jcolago.png?size=250",
+  },
 ];

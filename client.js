b2cdf85757f25757845ab062be5dd222f370d164
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-05 10:59:46 -0500

    made a for loop to loop through our people array

--- client.js
+++ client.js
@@ -3,10 +3,13 @@ $(onReady);
 console.log("Here are all the available people:", people);
 
 function onReady() {
-  console.log("hello world");
+  console.log(" in onReady");
 
   for (let i = 0; i < people.length; i++) {
     let person = people[i];
-    console.log(person[i]);
-  }
-}
+    //     $('#inBody').append(`<div id="images"><img src="${person.image}"
+    // </div>, <div id="joe></div>`)
+    console.log("in people for loop", person);
+    $("#inBody").append(person);
+  } // end forLoop
+} //end onReady

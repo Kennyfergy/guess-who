2b611c3f171f929f14b93b6fcb5b6c6f1637ec88
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-09-05 16:48:52 -0500

    finished base goals

--- client.js
+++ client.js
@@ -1,15 +1,44 @@
 $(onReady);
 
+const randomPerson = people[randomNumber()];
+
+// two inputs on the dom
+// on submit, take the data in the input and compare it to
+// randomPerson.name
+//
+
+console.log(randomPerson);
+
 console.log("Here are all the available people:", people);
 
 function onReady() {
   console.log(" in onReady");
+  addDiv();
+  $("#game").on("click", newFunction);
+} // end forLoop//end onReady
+function addDiv() {
+  for (i = 0; i < people.length; i++) {
+    $("#game").append(
+      `<div data-name=${people[i].name} id="images"><img src="${people[i].image}}"</div>`
+    );
+  }
+  $("#name").append(people[randomNumber()].name);
+  // console.log('random num', randomNumber());
+  // console.log('random name', people[randomNumber()].name);
+}
 
-  for (let i = 0; i < people.length; i++) {
-    let person = people[i];
-    //     $('#inBody').append(`<div id="images"><img src="${person.image}"
-    // </div>, <div id="joe></div>`)
-    console.log("in people for loop", person);
-    $("#inBody").append(person);
-  } // end forLoop
-} //end onReady
+function randomNumber() {
+  return Math.floor(Math.random() * (1 + 1 - 0) + 0);
+}
+
+function newFunction(event) {
+  let thing = $(event.target).closest("div");
+
+  console.log(thing.data().name);
+
+  if (thing.data().name === randomPerson.name) {
+    alert("Correct");
+  } else {
+    alert("Incorrect, please try again");
+  }
+}
